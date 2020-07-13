8f6adfcb2508a20dfd2baf6b081705e60cbc2c7a
Ivan_M <opana1988@yandex.ru>
2020-07-14 02:24:25 +0300

    complete homework for lesson7

---
+++ src/server/AuthService.java
@@ -0,0 +1,40 @@
+package server;
+
+import java.sql.*;
+
+public class AuthService {
+    private static Connection connection;
+    private static Statement statement;
+
+    public static void connect(){
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:main.db");
+            statement = connection.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getNickByLoginAndPass(String login, String pass) {
+        String sql = String.format("select nickname from main where login = '%s' and password = '%s'", login, pass);
+        try {
+            ResultSet rs = statement.executeQuery(sql);
+            if (rs.next()) {
+                return rs.getString(1);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static void disconnect(){
+        try {
+            connection.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+}
